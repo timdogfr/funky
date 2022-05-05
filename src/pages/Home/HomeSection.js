c308911a54d2f869857791048ed745d2aeb2ad32
timdogfr <103558704+timdogfr@users.noreply.github.com>
2022-05-05 15:27:43 +0300

    Update HomeSection.js

--- src/pages/Home/HomeSection.js
+++ src/pages/Home/HomeSection.js
@@ -25,11 +25,22 @@ const HomeSection = () => {
             <Grid item xs={12} sm={12} md={12} lg={12}>
               <Typography className={classes.title}>What is The Funky Horse Pub?</Typography>
               <Typography className={globalClasses.text21}>
-                The Funky Horse Pub is a collection of 8888 unique NFTs living on the Avalanche
-                blockchain. What makes this project different than any other NFT project is that there are collections
-                within the colletion. Of the 8888 Horses, The Funky Horse Pub community is the first NFT to NFT project in the world.The Funky Horses that the members have are in terrace, accessible by very loyal members!
-                On this platform, everyone can create and sell their own NFT works so that they can both earn pure income and strengthen our community.Another purpose of our platform is to create a connected and focused community by establishing a strong bond between people.
-                8.888 Horses is available, it will be issued on avalanche blockchain.
+                Welcome to Funkyworld. Funkyworld is a world where crazy and coolly dressed
+                Funky horses live. Funky horses spend most of their days partying, playing crazy
+                games, and drinking lemonade on Funky Horsepub, but they don't realize the great
+                danger that awaits them, and this danger is called Dr.Grumpy. Dr.Grumpy hates
+                everything that's fun, unlike Funkyhorses, he spends most of his day at parties, playing
+                games with lemonade and drinking strawberry wine with his cat by the fireplace and
+                also listening to classical music and sleeping early. Dr. Grumpy's house is a few
+                blocks away from Funky Horsepub, and one day there is a big party at Funky
+                Horsepub, they play crazy games and drink lemonade. The party is so loud that the
+                sound of the party is heard all over the Funky World, during which Dr.Grumpy is
+                sleeping soundly in his bed, Grumpy wakes up to the noise of the party and shouts at
+                them to turn off the music from his window, but the sound of the music is so loud that
+                no one hears it. Dr.Grumpy was angry about this. Grumpy can no longer tolerate the
+                parties held by Funky horses, he decides to destroy the Pub to bring a permanent
+                solution to this situation, for which he makes madbots in his workshop and sends
+                them to Funky Horsepub.
               </Typography>
             </Grid>
           </Box>
